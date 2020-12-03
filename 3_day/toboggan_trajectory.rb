cf970d27355be522dcc328b330a93a7b0349c377
Eduardo Parra <helloeduardo@icloud.com>
2020-12-02 23:03:17 -0700

    Add solution for part 2

--- 3_day/toboggan_trajectory.rb
+++ 3_day/toboggan_trajectory.rb
@@ -1,6 +1,3 @@
-# Array of Strings
-data = File.readlines('input.txt', chomp: true)
-
 class TobogganTrajectory
   attr_reader :data
 
@@ -8,16 +5,29 @@ class TobogganTrajectory
     @data = data
   end
 
-  def count_trees
+  def count_trees(right_shift, down_shift = 1)
     count = 0
-    data.each.with_index do |row, index|
-      if row[(3 * index) % row.size] == '#'
+    data.each_slice(down_shift).map(&:first).each.with_index do |row, index|
+      if row[(right_shift * index) % row.size] == '#'
         count += 1
       end
     end
     count
   end
+
+  def multiply_trees
+    count_trees(1) *
+    count_trees(3) *
+    count_trees(5) *
+    count_trees(7) *
+    count_trees(1, 2)
+  end
 end
 
-solution = TobogganTrajectory.new(data).count_trees
-p solution
+data = File.readlines('input.txt', chomp: true)
+
+solution_1 = TobogganTrajectory.new(data).count_trees(3)
+p solution_1
+
+solution_2 = TobogganTrajectory.new(data).multiply_trees
+p solution_2
