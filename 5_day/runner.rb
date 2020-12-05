bd0109fe8e5f708250741e20037c7ebe9842d74c
Eduardo Parra <helloeduardo@icloud.com>
2020-12-04 23:21:12 -0700

    Re-org file structure to include lib and data

--- 5_day/runner.rb
+++ 5_day/runner.rb
@@ -1,10 +1,10 @@
-require "./binary_boarding"
-require "./boarding_pass"
+require "./lib/binary_boarding"
+require "./lib/boarding_pass"
 
-data = File.readlines('input.txt', chomp: true)
+data = File.readlines('data/input.txt', chomp: true)
 
 solution_1 = BinaryBoarding.new(data).highest_seat_id
-p solution_1
+p "Solution 1: #{solution_1}"
 
 solution_2 = BinaryBoarding.new(data).my_seat_id
-p solution_2
+p "Solution 2: #{solution_2}"
