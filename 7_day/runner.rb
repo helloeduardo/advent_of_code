e47b553d79441ef3abb4bbde37b19ca43be0b460
Eduardo Parra <helloeduardo@icloud.com>
2020-12-07 21:37:02 -0700

    Add comment

--- 7_day/runner.rb
+++ 7_day/runner.rb
@@ -4,7 +4,7 @@ require "./lib/bag"
 data = File.readlines('data/input.txt', chomp: true)
 
 solution_1 = LuggageProcessor.new(data).count_of_bags_containing("shiny gold")
-p "Solution 1: #{solution_1}" #233
+p "Solution 1: #{solution_1}" #233 using input
 
 solution_2 = LuggageProcessor.new(data).count_of_bags_contained_in("shiny gold")
-p "Solution 2: #{solution_2}"
+p "Solution 2: #{solution_2}" #421550 using input
